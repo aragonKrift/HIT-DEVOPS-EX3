fca1c5146fdb4d2cee520f91302c717e452253a6
Nir <nir.tsameret@gmail.com>
2024-05-20 10:20:45 +0300

    Change namefile

--- NirTsameret.java
+++ NirTsameret.java
@@ -1,4 +1,4 @@
-public class MyName {
+public class NirTsameret {
     public static void main(String[] args) {
         for (int i = 0; i < 7; i++) {
             System.out.println("Nir Tsameret");
