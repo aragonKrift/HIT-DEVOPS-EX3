b7d3a14da4388e2656185c04711f814046369a21
Nir <nir.tsameret@gmail.com>
2024-05-20 10:28:24 +0300

    Modified NirTsameret

--- NirTsameret.java
+++ NirTsameret.java
@@ -1,7 +1,7 @@
 public class NirTsameret {
     public static void main(String[] args) {
         for (int i = 0; i < 7; i++) {
-            System.out.println("Nir Tsameret");
+            System.out.println("Different print");
         }
     }
 }
